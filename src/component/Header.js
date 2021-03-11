ac98a1a444c55a3b1b8dc199294a7b1dc3fac561
Wisnu Box <46359374+hwisnu222@users.noreply.github.com>
2021-03-11 11:40:03 +0700

    add register

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,10 +6,15 @@ import FoodWays from "../assets/icon/foodways.svg";
 import Logo from "../assets/icon/logo.svg";
 
 export default function Header() {
-  const [modalShow, setModalShow] = useState(false);
+  const [modalLogin, setModalLogin] = useState(false);
+  const [modalRegister, setModalRegister] = useState(false);
 
-  const handleModal = () => {
-    setModalShow(!modalShow);
+  const handleModalLogin = () => {
+    setModalLogin(!modalLogin);
+  };
+
+  const handleModalRegister = () => {
+    setModalRegister(!modalRegister);
   };
 
   return (
@@ -20,19 +25,21 @@ export default function Header() {
           <img src={Logo} alt="logo" />
         </Navbar.Brand>
         <Form inline>
-          <Button variant="dark mr-2" onClick={handleModal}>
+          <Button variant="dark mr-2" onClick={handleModalRegister}>
             Register
           </Button>
-          <Button variant="dark">Login</Button>
+          <Button variant="dark" onClick={handleModalLogin}>
+            Login
+          </Button>
         </Form>
       </Navbar>
-      {/* modal */}
+      {/* modal login*/}
       <Modal
         size="sm"
         aria-labelledby="contained-modal-title-vcenter"
         centered
-        show={modalShow}
-        onHide={handleModal}
+        show={modalLogin}
+        onHide={handleModalLogin}
       >
         <Modal.Header closeButton className="border-0">
           <h3 className="text-yellow">Login</h3>
@@ -50,6 +57,49 @@ export default function Header() {
             </Button>
             <p className="text-center opacity-50 mt-2">
               Don't have an account ? Klik Here
+            </p>
+          </Form>
+        </Modal.Body>
+      </Modal>
+      {/* modal register */}
+      <Modal
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        show={modalRegister}
+        onHide={handleModalRegister}
+      >
+        <Modal.Header closeButton className="border-0">
+          <h3 className="text-yellow">Register</h3>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Control type="email" placeholder="Email" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="text" placeholder="Full Name" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="text" placeholder="Gender" />
+            </Form.Group>
+            <Form.Group>
+              <Form.Control type="text" placeholder="Phone" />
+            </Form.Group>
+            <Form.Group controlId="exampleForm.ControlSelect1">
+              <Form.Control as="select">
+                <option>Customer</option>
+                <option>Partner</option>
+              </Form.Control>
+            </Form.Group>
+            <Button className="brown mt-4" block>
+              Login
+            </Button>
+            <p className="text-center opacity-50 mt-2">
+              Already have an account ? Klik Here
             </p>
           </Form>
         </Modal.Body>
